e33dc949db89bc3572114fe7c9fd4010afedab47
Fraz <diman896@gmail.com>
2017-10-22 22:13:30 +0300

    Configure database connection.

---
+++ AutoserviceOnlineServer/AutoserviceOnlineServer/Controllers/CarsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DataAccess;
+using DataAccess = DataAccess.DataAccess;
+
+namespace AutoserviceOnlineServer.Controllers
+{
+    public class CarsController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            //return new List<string>()
+            //{
+            //    "test",
+            //    "test2"
+            //};
+            var db = global::DataAccess.DataAccess.GetContext();
+            var casrs = db.car.ToList();
+            return casrs.Select(x => x.model.ToString());
+        }
+    }
+}
--- AutoserviceOnlineServer/AutoserviceOnlineServer/Controllers/ValuesController.cs
+++ AutoserviceOnlineServer/AutoserviceOnlineServer/Controllers/ValuesController.cs
@@ -11,7 +11,7 @@ namespace AutoserviceOnlineServer.Controllers
     {
         // GET api/<controller>
         public IEnumerable<string> Get()
-        {
+        { 
             return new string[] { "value1", "value2" };
         }
 
